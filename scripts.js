bee60602b977d59080c56c28ac15fb7aabe4f772
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-02 18:12:34 -0300

    Dev.finance$

--- scripts.js
+++ scripts.js
@@ -14,6 +14,38 @@ const Modal = {
           .querySelector('.modal-overlay')
           .classList
           .remove('active');
+    }
+}
+
+const transactions = [{
+  id: 1,
+  description: 'Luz',
+  amount: 50000
+
+}]
+
+// Eu preciso somar as entradas
+// Depois eu preciso somar as saídas e
+// Remover das entradas os valores das saídas
+// Assim, eu terei o total
+
+const Transaction = {  // "=" Atribuindo valor
+    incomes(){
+      // somar as entradas
+
+
+    },
+    expenses(){
+      // somar as saídas
+
+
+    },
+    total(){
+      // entradas - saídas
+
 
     }
-  }
+
+
+
+}
