081743594fae627a74df6221441aa52df841c9a2
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-06 19:08:00 -0300

    Dev.finance$

--- scripts.js
+++ scripts.js
@@ -164,11 +164,19 @@ const Utils = {
 }
 
 const Form = {
-  formatData(){
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
 
+  getValues(){
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }
   },
   validateFields(){
-
+    
   },
   submt(event){
     event.preventDefault()
